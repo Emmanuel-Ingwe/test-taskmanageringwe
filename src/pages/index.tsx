a5674867d5f6685fe04f083efc0ff28c54f5ce27
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-11-27 08:34:11 +0100

    ..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
-  const [todos, setTodos] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState("");
-  
-  const handleInputValue = (event:React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value)
-  }
 
-  const handleAddTodo = () => {
-    if (inputValue.trim() !== '') {
-      setTodos([...todos, inputValue]);
-      setInputValue('');
-    }
-  };
-
-  const deleteTodo = (index: number) => {
-      const updatedTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
-      setTodos(updatedTodos);
-  }
 
 
   return (
@@ -32,24 +15,16 @@ export default function Home() {
           <div className="uppercase text-green-500 mb-10"><h1 className='text-2xl'>Todo App <span className='text-yellow-500'>2.0</span></h1></div>
 
           <div className="">
-            <input type="text" placeholder='type todo' value={inputValue} onChange={handleInputValue} className='p-3 rounded outline-none text-black' />
+            <input type="text" placeholder='type todo' className='p-3 rounded outline-none text-black' />
 
             <button
              type="submit"
             className='bg-blue-500 p-3 rounded m-2'
-            onClick={handleAddTodo}
+            // onClick={}
             >add todo</button>
 
             <ul className='m-2 bg-cyan-400 rounded p-3'>
-              {todos.map((todo, index) => (
-                <li className='flex items-center justify-between'>{todo}
-                  <button
-                  type="submit"
-                  className='bg-red-700 p-3 rounded m-1'
-                  onClick={() => deleteTodo(index)}
-                  >del todo</button>
-                </li>
-              ))}
+              
             </ul>
 
           </div>
