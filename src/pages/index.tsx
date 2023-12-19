c65b2e7868b83eae00763d36c59683aa3f90b141
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-12-19 11:34:02 +0100

    ..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,37 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [todo, setTodo] = useState();
+
   return (
     <>
-      <section>
-        <div className=""><h1>hello world</h1></div>
+      <section className='flex items-center justify-center'>
+        <div className="uppercase"><h1>hello world</h1></div>
+
+        <div className="">
+          <input type="text" />
+          <button type="submit">add todo</button>
+        </div>
       </section>
     </>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// first, call out the usestates functions
+// then the addTodo function
+// the todoInput function
+// deleteTodo function
