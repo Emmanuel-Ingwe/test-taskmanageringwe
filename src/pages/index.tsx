4f75c500d3234f2da13d8590fd35d4bafddf65e2
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-12-19 11:59:55 +0100

    onClicks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,18 @@ export default function Home() {
 
   return (
     <>
-      <section className='flex items-center justify-center'>
-        <div className="uppercase"><h1>hello world</h1></div>
+      <section className='flex items-center justify-center '>
+        <div className="flex flex-col items-center justify-center">
+          <div className="uppercase text-green-500 mb-10"><h1>Todo App <span className='text-yellow-500'>2.0</span></h1></div>
 
-        <div className="">
-          <input type="text" />
-          <button type="submit">add todo</button>
+          <div className="">
+            <input type="text" placeholder='type todo' className='p-3 rounded outline-none text-black' />
+            <button
+             type="submit"
+            className='bg-blue-500 p-3 rounded m-2'
+            // onClick={}
+            >add todo</button>
+          </div>
         </div>
       </section>
     </>
