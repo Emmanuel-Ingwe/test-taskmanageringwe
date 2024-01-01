9b48822ad997d43234f919f24e30e6d8c07b6051
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-01-01 20:01:14 +0100

    ..

--- component/form.tsx
+++ component/form.tsx
@@ -1,39 +1,39 @@
-import * as React from "react"
-import { useForm } from "react-hook-form" 
+import { useForm, SubmitHandler } from "react-hook-form"
 
 
-type FormData = {
-  firstName: boolean,
-  lastName: string
+type Inputs = {
+  example: string
+  exampleRequired: string
 }
 
 
 export default function App() {
   const {
     register,
-    setValue,
     handleSubmit,
+    watch,
     formState: { errors },
-  } = useForm<FormData>()
-  const onSubmit = handleSubmit((data) => console.log(data))
-  // firstName and lastName will have correct type
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+
+  console.log(watch("example")) // watch input value by passing the name of it
 
 
   return (
-    <form onSubmit={onSubmit}>
-      <label>First Name</label>
-      <input {...register("firstName")} />
-      <label>Last Name</label>
-      <input {...register("lastName")} />
-      <button
-        type="button"
-        onClick={() => {
-          setValue("lastName", "luo") // ✅
-          setValue("firstName", true) // ❌: true is not string
-        }}
-      >
-        submit
-      </button>
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+
+      <input type="submit" />
     </form>
   )
 }
